638fc7eb938607fa8e7ae126bc1a504f91339902
gauravaps <gauravapsout@look.com>
2024-01-26 23:55:13 +0530

    factorial

--- backend/index.js
+++ backend/index.js
@@ -5,4 +5,28 @@
 // git branch -M main
 // git remote add origin https://github.com/gauravaps/shoppingCart.git
 // git push -u origin maingit init
-console.log('this is console');
+// console.log('this is console');
+// let num =5
+//  let fact=1
+// for(let i=1;i<=num;i++){
+//     fact*=i
+  
+  
+
+
+// }
+//console.log(fact);
+// Define the number whose factorial you want to calculate
+const number = 5;
+let factorial = 1;
+
+// Calculate the factorial using a for loop
+for (let i = 1; i <= number; i++) {
+    
+    factorial *= i;
+    
+console.log("Factorial of", number + ":", factorial);
+}
+
+// // Print the factorial
+// console.log("Factorial of", number + ":", factorial);
