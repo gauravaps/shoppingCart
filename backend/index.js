7fb684666af851d063ace03bda8f5d483ca235c0
gauravaps <gauravapsout@look.com>
2024-01-26 23:08:05 +0530

    this is test

--- backend/index.js
+++ backend/index.js
@@ -4,4 +4,5 @@
 // git commit -m "first commit"
 // git branch -M main
 // git remote add origin https://github.com/gauravaps/shoppingCart.git
-// git push -u origin main
+// git push -u origin maingit init
+console.log('this is console');
